9a3c6739e475f4d1df13b33395ac1bface75891e
natnaelbiki <96464327+natnaelbiki@users.noreply.github.com>
2022-09-29 14:58:34 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="django-simple-chatbot",
+    version="0.0.9",
+    author="Janga",
+    license='MIT License',
+    author_email="jangascodingplace@gmail.com",
+    description="A very basic Django Chatbot ft. NLTK and DRF",
+    url="https://github.com/Codingplace42/django-simple-chatbot",
+    packages=find_packages(),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'django>=2.2',
+        'nltk',
+        'djangorestframework'
+    ]
+)
